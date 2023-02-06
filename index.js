e83382e36d3af972079ecfeb511d2fca0dd8440f
Kamryn Smith <programersign@gmail.com>
2023-02-06 11:14:09 -0800

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+const ralph = "Ralph";
+const bob = "Bob";
+const newCats = [
+    ...cats.slice(0,3),
+    "Broom",
+];
+const newCatsTwo = [
+    "Arnold",
+    ...cats.slice(0,3),
+]
+
+const newCatsThree = [
+    ...cats.slice(1,3),
+]
+
+const newCatsFour = [
+    ...cats.slice(0,2)
+]
+
+//Functions:
+
+function destructivelyAppendCat(ralph) {
+    cats.push(ralph);
+}
+
+function destructivelyPrependCat(bob) {
+    cats.unshift(bob);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    return newCats;
+}
+
+function prependCat() {
+    return newCatsTwo;
+}
+
+function removeFirstCat() {
+    return newCatsThree;
+}
+
+function removeLastCat() {
+    return newCatsFour;
+}
+
